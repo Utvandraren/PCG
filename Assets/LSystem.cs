15cdc0fa370fd6e0734b9c5c6fc7ab74451fa9db
Utvandraren <38995092+Utvandraren@users.noreply.github.com>
2020-09-26 21:42:16 +0200

    added all files

---
+++ Assets/LSystem.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LSystem : MonoBehaviour
+{
+    [SerializeField] private int iterations = 1;
+    [SerializeField] private Alphabet[] grammar;
+
+    Vector3 currentPosition;
+    string generatedObjects = "a";
+    string tempGeneratedObjects = "";
+
+
+    [System.Serializable]
+    public struct Alphabet
+    {
+        public char startSymbol;
+        public string rule;
+        public GameObject objToInstantiate;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPosition = transform.position;
+        Generate();
+    }
+
+    void Generate() //Go through current instructions for what to do
+    {
+        for (int i = 0; i < iterations; i++)
+        {
+            foreach (char letter in generatedObjects)
+            {
+                Interpret(letter);
+            }
+            generatedObjects += tempGeneratedObjects;
+            tempGeneratedObjects = "";
+            WriteDebugMessage();
+            GenerateObjects();
+        }       
+    }
+
+    void Interpret(char letter)  //Translate the current letter to the next instructions
+    {
+        for (int i = 0; i < grammar.Length; i++)
+        {
+            if (letter == grammar[i].startSymbol)
+            {
+                foreach (char result in grammar[i].rule)
+                {
+                    tempGeneratedObjects += result;
+                }
+            }
+        }
+    }
+
+    void GenerateObjects() //Generate phenotypes based on the gentypes generated
+    {
+        foreach (char letter in generatedObjects)
+        {
+            for (int i = 0; i < grammar.Length; i++)
+            {
+                if(letter == grammar[i].startSymbol)
+                {
+                    Instantiate(grammar[i].objToInstantiate, currentPosition, Quaternion.identity);
+                    currentPosition.x += 2f;
+                }
+            }
+        }
+        currentPosition.z += 1f;
+    }
+
+    void WriteDebugMessage()
+    {
+        Debug.Log("Iteration:" + iterations.ToString() + "||" + generatedObjects);
+    }
+
+}
