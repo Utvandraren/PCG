debfda72295d3df286c8f0831f6402d9b91027f8
Utvandraren <38995092+Utvandraren@users.noreply.github.com>
2020-10-12 23:21:11 +0200

    added beginning to evolutionary algortihm code

---
+++ Assets/GeneticAlgorithm.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GeneticAlgorithm : MonoBehaviour
+{
+    string genotype;
+    List<string> createdGrammar;
+    List<string> candidates;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        createdGrammar = new List<string>();
+        candidates = new List<string>();
+    }
+
+   
+    /// <summary>
+    /// Evolve a new generation of grammar rules based on fitnessfunctions supplied to it
+    /// </summary>
+    public void EvolveGrammar()
+    {
+        
+
+        foreach(string grammar in createdGrammar)
+        {
+            if (FitnessFunction())
+            {
+                candidates.Add(grammar);
+            }
+        }
+    }
+
+    void Permutate()
+    {
+
+    }
+
+    public bool FitnessFunction()
+    {
+        return true;
+    }
+
+    public void AddGrammar(string value)
+    {
+        createdGrammar.Add(value);
+    }
+}
--- Assets/GrammarGenerator.cs
+++ Assets/GrammarGenerator.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class GrammarGenerator : MonoBehaviour
 {
     System.Random rand = new System.Random();
-
+    GeneticAlgorithm test;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
- 
+
     }
 
     // Update is called once per frame
